095c0a5a8f8ee74353ebff1bddfe1f54903a5174
clx1608@qq.com <hbdennischen@outlook.com>
2018-06-14 11:00:45 +0800

    use mongo session store

--- server.js
+++ server.js
@@ -1,47 +1,52 @@
 require('dotenv').config();
 
 const path = require('path'),
-    cors = require('cors'),
-    cookieParser = require('cookie-parser'),
-    session = require('express-session'),
-    uuid = require('uuid-v4'),
-    SocketIo = require('socket.io'),
-    PassportSocketIo = require('passport.socketio'),
-    NedbStore = require('nedb-session-store'),
-    restsDir = path.join(__dirname, './server/rests'),
-    resourceDescriptors = require('@finelets/hyper-rest/rests/DirectoryResourceDescriptorsLoader').loadFrom(restsDir),
-    resourceRegistry = require('@finelets/hyper-rest/rests/ResourceRegistry'),
-    graph = require('./server/flow'),
-    transitionsGraph = require('@finelets/hyper-rest/rests/BaseTransitionGraph')(graph, resourceRegistry),
-    connectDb = require('@finelets/hyper-rest/db/mongoDb/ConnectMongoDb'),
-    // sessionStore = require('@finelets/hyper-rest/session/MongoDbSessionStore')(1000 * 60 * 60 * 24), // set session for 1 day
-    appBuilder = require('@finelets/hyper-rest/express/AppBuilder').begin(__dirname),
-    passport = require('./server/authGithub'),
-    CLIENT_ORIGIN = process.env.CLIENT_ORIGIN,
-    SECRET = process.env.SESSION_SECRET,
-    corsOptions = {
-        origin: CLIENT_ORIGIN,
-        credentials: true
-    },
-    logger = require('@finelets/hyper-rest/app/Logger');
+	cors = require('cors'),
+	cookieParser = require('cookie-parser'),
+	session = require('express-session'),
+	uuid = require('uuid-v4'),
+	SocketIo = require('socket.io'),
+	PassportSocketIo = require('passport.socketio'),
+	//NedbStore = require('nedb-session-store'),
+	MongodbStore = require('connect-mongo'),
+	restsDir = path.join(__dirname, './server/rests'),
+	resourceDescriptors = require('@finelets/hyper-rest/rests/DirectoryResourceDescriptorsLoader').loadFrom(restsDir),
+	resourceRegistry = require('@finelets/hyper-rest/rests/ResourceRegistry'),
+	graph = require('./server/flow'),
+	transitionsGraph = require('@finelets/hyper-rest/rests/BaseTransitionGraph')(graph, resourceRegistry),
+	connectDb = require('@finelets/hyper-rest/db/mongoDb/ConnectMongoDb'),
+	// sessionStore = require('@finelets/hyper-rest/session/MongoDbSessionStore')(1000 * 60 * 60 * 24), // set session for 1 day
+	appBuilder = require('@finelets/hyper-rest/express/AppBuilder').begin(__dirname),
+	passport = require('./server/authGithub'),
+	CLIENT_ORIGIN = process.env.CLIENT_ORIGIN,
+	SECRET = process.env.SESSION_SECRET,
+	corsOptions = {
+		origin: CLIENT_ORIGIN,
+		credentials: true
+	},
+	logger = require('@finelets/hyper-rest/app/Logger');
 
-const NedbSessionStore = NedbStore(session);
+/* const NedbSessionStore = NedbStore(session);
 const sessionStore = new NedbSessionStore({
     filename: path.join('./db', 'session-store.db')
+}); */
+const MongodbSessionStore = MongodbStore(session);
+const sessionStore = new MongodbSessionStore({
+	url: process.env.MONGODB
 });
 const sessionOptions = {
-    genid: function () {
-        return uuid();
-    },
-    key: 'express.sid',
-    secret: SECRET,
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-        maxAge: 3 * 60 * 60 * 1000,
-        secure: process.env.NODE_ENV === 'production',
-    },
-    store: sessionStore,
+	genid: function() {
+		return uuid();
+	},
+	key: 'express.sid',
+	secret: SECRET,
+	resave: true,
+	saveUninitialized: true,
+	cookie: {
+		maxAge: 3 * 60 * 60 * 1000,
+		secure: process.env.NODE_ENV === 'production'
+	},
+	store: sessionStore
 };
 
 resourceRegistry.setTransitionGraph(transitionsGraph);
@@ -53,12 +58,9 @@ app.use(session(sessionOptions));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get(
-    '/api/auth/callback',
-    passport.authenticate('github'),
-    function (req, res) {
-        logger.debug('auth success! next, we will post a success message to client orgin!');
-        res.send(`<html>
+app.get('/api/auth/callback', passport.authenticate('github'), function(req, res) {
+	logger.debug('auth success! next, we will post a success message to client orgin!');
+	res.send(`<html>
     <body>
       <script>
         window.opener.postMessage('success', '${CLIENT_ORIGIN}')
@@ -67,31 +69,32 @@ app.get(
       Success!
     </body>
     </html>`);
-    }
-);
+});
 
 appBuilder
-    .setResources(resourceRegistry, resourceDescriptors)
-    .setWebRoot('/root', './client')
-    .setFavicon('client/images/favicon.jpg')
-    // .setSessionStore(sessionStore)
-    .end();
+	.setResources(resourceRegistry, resourceDescriptors)
+	.setWebRoot('/root', './client')
+	.setFavicon('client/images/favicon.jpg')
+	// .setSessionStore(sessionStore)
+	.end();
 
-connectDb(function () {
-    logger.info('connect mongodb success .......');
-    var server = appBuilder.run(function () {
-        const io = SocketIo(server);
-        io.use(PassportSocketIo.authorize({
-            cookieParser: cookieParser,
-            key: 'express.sid',
-            secret: SECRET,
-            store: sessionStore,
-            success: function (data, accept) {
-                logger.info('socket.io auth success');
-                accept();
-            },
-        }));
-        var addr = server.address();
-        logger.info('the server is running and listening at ' + addr.port);
-    });
+connectDb(function() {
+	logger.info('connect mongodb success .......');
+	var server = appBuilder.run(function() {
+		const io = SocketIo(server);
+		io.use(
+			PassportSocketIo.authorize({
+				cookieParser: cookieParser,
+				key: 'express.sid',
+				secret: SECRET,
+				store: sessionStore,
+				success: function(data, accept) {
+					logger.info('socket.io auth success');
+					accept();
+				}
+			})
+		);
+		var addr = server.address();
+		logger.info('the server is running and listening at ' + addr.port);
+	});
 });
