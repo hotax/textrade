923e5e2697aa1387b8dea22b54aecd8e0e13ee3e
clx1608@qq.com <hbdennischen@outlook.com>
2018-06-22 13:50:48 +0800

    add auth logout

--- server/rests/AuthLogout.js
+++ server/rests/AuthLogout.js
@@ -1,10 +1,12 @@
 module.exports = {
-    url: '/api/logout',
-    rests: [{
-        type: 'get',
-        handler: function (req, res) {
-            req.logout();
-            res.json({ status: 'ok' });
-        }
-    }]
+	url: '/api/logout',
+	rests: [
+		{
+			type: 'get',
+			handler: function(req, res) {
+				req.logout();
+				res.json({ status: 'ok' });
+			}
+		}
+	]
 };
