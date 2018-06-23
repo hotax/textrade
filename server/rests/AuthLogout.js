2ed2f62d8e72dfcdd0add38216e4123613cb1896
clx1608@qq.com <hbdennischen@outlook.com>
2018-06-23 22:26:08 +0800

    refactor

--- server/rests/AuthLogout.js
+++ server/rests/AuthLogout.js
@@ -1,12 +1,14 @@
 module.exports = {
 	url: '/api/logout',
-	rests: [{
-		type: 'get',
-		handler: function (req, res) {
-			req.logout();
-			res.json({
-				status: 'ok'
-			});
+	rests: [
+		{
+			type: 'get',
+			handler: function(req, res) {
+				req.logout();
+				res.json({
+					status: 'ok'
+				});
+			}
 		}
-	}]
+	]
 };
