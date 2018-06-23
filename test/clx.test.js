368e60861386c08ffbe23038fd525d9364528ca8
clx1608@qq.com <hbdennischen@outlook.com>
2018-06-24 00:00:54 +0800

    tdd

--- test/clx.test.js
+++ test/clx.test.js
@@ -1,33 +1,33 @@
 var proxyquire = require('proxyquire'),
-    dbSave = require('@finelets/hyper-rest/db/mongoDb/SaveObjectToDb');
+	dbSave = require('@finelets/hyper-rest/db/mongoDb/SaveObjectToDb');
 
-describe('Application', function () {
-    var func, stubs, err, reason, createReasonStub;
-    before(function () {
-        mongoose.Promise = global.Promise;
-    });
+describe('Application', function() {
+	var func, stubs, err, reason, createReasonStub;
+	before(function() {
+		mongoose.Promise = global.Promise;
+	});
 
-    beforeEach(function () {
-        stubs = {};
-        err = new Error('any error message');
-        reason = {
-            reason: 'any reason representing any error'
-        };
-        createReasonStub = sinon.stub();
-        stubs['@finelets/hyper-rest/app'] = {
-            createErrorReason: createReasonStub
-        };
-    });
+	beforeEach(function() {
+		stubs = {};
+		err = new Error('any error message');
+		reason = {
+			reason: 'any reason representing any error'
+		};
+		createReasonStub = sinon.stub();
+		stubs['@finelets/hyper-rest/app'] = {
+			createErrorReason: createReasonStub
+		};
+	});
 
-    describe('数据库', function () {
-        beforeEach(function (done) {
-            if (mongoose.connection.db) return done();
-            mongoose.connect(dbURI, done);
-        });
+	describe('数据库', function() {
+		beforeEach(function(done) {
+			if (mongoose.connection.db) return done();
+			mongoose.connect(dbURI, done);
+		});
 
-        it('aass', function () {
-            var x = 1;
-            expect(x).eqls(1);
-        });
-    });
+		it('aass', function() {
+			var x = 1;
+			expect(x).eqls(1);
+		});
+	});
 });
